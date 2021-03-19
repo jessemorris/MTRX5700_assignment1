38403d09aa70403d81daf79dbcb9b2605cbee2be
unknown <mzak8780@uni.sydney.edu.au>
2021-03-19 16:51:31 +1100

    Corrected the input angles to achieve the desired position of the robot arm

--- question_1/forward.py
+++ question_1/forward.py
@@ -2,18 +2,28 @@ import numpy as np
 import math
 from visual_kinematics import *
 
-dh_params = np.array([[0, 0, 0, 0],
-                      [ 0.163, 0, 0, -0.5 * math.pi ],
+
+dh_params = np.array([[ 0.163, 0, 0, -0.5 * math.pi ],
                       [ 0.007, 0.25 * math.pi, -0.425, 0],
                       [ 0, -0.5 * math.pi, 0.392, 0],
-                      [ 0.127, 0.5 * math.pi,  0, 0.5 * math.pi],
-                      [  0.1, 0, 0, -0.5 * math.pi],
+                      [ 0.127, 0,  0, -0.5 * math.pi],
+                      [  0.1, 0 ,0, 0.5 * math.pi],
                       [ 0.1, 0, 0, 0]])
+
 
 robot = Robot(dh_params)
 
-# theta = np.array([0., -0.5 * math.pi, 0 , -0.5*math.pi, 0., 0., 0.])
-theta = np.array([0, 0, 0 , 0, 0., 0., 0.])
+# Uncomment one of the following to select a viewing state
+
+###### Part c, straight up position ########
+# theta = np.array([0., 0.25 * math.pi, -0.5*math.pi, -0.5*math.pi , 0., 0])
+
+######### Initial state position ##########
+# theta = np.array([0, 0, 0 , 0, 0, 0])
+
+########## 0 state position ###############
+# theta = np.array([0, 0.75*math.pi, -0.5*math.pi, 0 ,0, 0])
+
 f = robot.forward(theta)
 
 robot.show()
